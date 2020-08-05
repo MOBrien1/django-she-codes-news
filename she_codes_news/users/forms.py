d013261f9ce42d41fdb193d8f23b2f4b4c375978
MegsCodes <67173712+MOBrien1@users.noreply.github.com>
2020-08-05 20:52:09 +1000

    new user app

---
+++ she_codes_news/users/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import CustomUser
+
+
+class CustomUserCreationForm(UserCreationForm):
+
+    class meta:
+        model = CustomUser
+        fields = ['username', 'email']
+
+
+
+
+class CustomUserChangeForm(UserChangeForm):
+
+    class meta:
+        model = CustomUser
+        fields = ['username', 'email']
--- she_codes_news/users/models.py
+++ she_codes_news/users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class CustomUser(AbstractUser):
+
+    pass #this is where I would add my own fields i.e. profile pic
+    def __str__(self):
+        return self.username
